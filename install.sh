4b0c71c5fa19149ba0f24a79729393b4d5e4c6c4
yifattih <yifattih@proton.me>
2025-06-22 17:45:06 -0500

    feat(installer): introduce install.sh script to install packages and manage configurations
    This is the entrypont of my dotfiles.

---
+++ install.sh
@@ -0,0 +1,163 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd -P)
+
+usage() {
+    cat << EOF # remove the space between << and EOF, this is due to web plugin issue
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-f] -p param_value arg1 [arg2...]
+
+Script description here.
+
+Available options:
+
+-h, --help      Print this help and exit
+-v, --verbose   Print script debug info
+-f, --flag      Some flag description
+-p, --param     Some param description
+EOF
+    exit
+}
+
+cleanup() {
+    trap - SIGINT SIGTERM ERR EXIT
+    # script cleanup here
+}
+
+setup_colors() {
+    if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+        NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+        NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+log() {
+    if [ "${#}" -eq 1 ]; then
+        local lvl="INFO"
+        local msg="${1}"
+    else
+        local lvl="${1}"
+        local msg="${2}"
+    fi
+    echo "$(date +'%H:%M:%S') [${lvl}] - ${msg}"
+}
+
+die() {
+    if [ "${#}" -eq 1 ]; then
+        local lvl="ERROR"
+        local msg="${1}"
+        local code=1
+    else
+        local lvl="${1}"
+        local msg="${2}"
+        local code="${3}"
+    fi
+    log "${lvl}" "${msg}"
+    exit "${code}"
+}
+
+parse_params() {
+    pkgs=0
+    snaps=0
+    file=0
+
+    while :; do
+        case "${1-}" in
+            -h | --help) usage ;;
+            -v | --verbose) set -x ;;
+            --no-color) NO_COLOR=1 ;;
+            -p | --packages) pkgs=1 ;;
+            -s | --snaps) snaps=1 ;;
+            -f | --file) file=1 ;;
+            -?*) die "Unknown option: ${1}" ;;
+            *) break ;;
+        esac
+        shift
+    done
+
+    log "Starting up..."
+
+    if [ ${pkgs} -eq 0 ] && [ ${snaps} -eq 0 ] && [ ${file} -eq 0 ]; then
+        log "WARN" "Script running as 'symlinks' farm manager only"
+	log "WARN" "It will fail if required package is not installed"
+	log "WARN" "Please, see ./install.sh --help for usage information"
+    fi
+
+    if [ ${pkgs} -eq 1 ] && [ ! -f "${script_dir}"/packages/apt-packages.txt ]; then
+        die "File 'apt-packages' not found in script directory"
+    fi
+
+    if [ ${snaps} -eq 1 ] && [ ! -f "${script_dir}"/packages/snap-packages.txt ]; then
+        die "File 'snap-pakcages' not found in script directory"
+    fi
+
+    if [ ${file} -eq 1 ] && [ ! -f "${script_dir}"/packages/external-packages.sh ]; then
+        die "File 'external-packages' not found in script directory"
+    fi
+
+    log "Parameters parsed successfully"
+
+    return 0
+}
+
+parse_params "${@}"
+setup_colors
+
+install_apt_packages() {
+    log "Installing apt packages"
+    while IFS=: read -r pkg bin; do
+        # Skip blank lines and comments
+        [[ -z "${pkg}" || "${pkg}" == \#* ]] && continue
+        # If 'bin' is empty, default to 'pkg'
+        bin="${bin:-${pkg}}"
+        if ! command -v "${bin}" > /dev/null 2>&1; then
+            log "Installing package: ${pkg}"
+            sudo apt-get install "${pkg}" -y > /dev/null 2>&1
+            log "'${pkg}' installed successfully"
+        else
+            log "Package '${pkg}' is already installed"
+        fi
+    done < "${script_dir}/packages/apt-packages.txt"
+    log "SUCCESS" "Apt packages installation complete"
+}
+
+install_snap_packages() {
+    log "Installing snap packages"
+    while IFS=: read -r snap; do
+        # Skip blank lines and comments
+        [[ -z "${snap}" || "${snap}" == \#* ]] && continue
+        if ! snap list | grep -q "^${snap}\s" > /dev/null 2>&1; then
+            log "Installing package: ${snap}"
+            sudo snap install "${snap}" --classic > /dev/null 2>&1
+            log "'${snap}' installed successfully"
+        else
+            log "Package '${snap}' is already installed"
+        fi
+    done < "${script_dir}/packages/snap-packages.txt"
+    log "SUCCESS" "Snap packages installation complete"
+}
+
+install_external_packages() {
+    log "Installing other packages from file"
+    bash "${script_dir}/packages/external-packages.sh"
+    log "SUCCESS" "Other packages installation complete"
+}
+
+log "Updating system..."
+sudo apt-get update > /dev/null 2>&1
+log "Update completed"
+
+log "Upgrading system"
+sudo apt-get upgrade -y > /dev/null 2>&1
+log "System upgrade completed"
+
+if [ ${pkgs} -eq 1 ]; then install_apt_packages; fi
+if [ ${snaps} -eq 1 ]; then install_snap_packages; fi
+if [ ${file} -eq 1 ]; then install_external_packages; fi
+
+log "Setting up dotfiles using stow..."
+stow -R --no-folding --verbose=2 --target="${HOME}" .
+
+die "SUCCESS" "Dotfiles and packages installation complete" 0
