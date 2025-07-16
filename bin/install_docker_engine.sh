ffde2e7d466ddc9976a9239719dd11f8bb7e317b
yifattih <yifattih@proton.me>
2025-07-16 16:08:14 -0500

    ref(installer): move github cli installation from install-external-packages to its own module for modularization and remove install-external-packages that is no longer needed

---
+++ bin/install_docker_engine.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)
+
+usage() {
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v]
+
+Docker Engine installer script.
+
+Available options:
+
+-h, --help                          Print this help and exit
+-v, --verbose                       Print script debug info
+EOF
+  exit
+}
+
+run_cmd() {
+  if [ "${VERBOSE:-0}" -eq 1 ]; then
+    "${@}"
+  else
+    "${@}" 2>/dev/null
+  fi
+}
+
+safe_rm() {
+  rm -rf "${1}" 2>/dev/null || sudo rm -rf "${1}" 2>/dev/null || log "WARN" "Failed to remove ${1}"
+}
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+}
+
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' \
+      BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+log() {
+  if [ "${#}" -eq 1 ]; then
+    local lvl="INFO"
+    local msg="${1}"
+  else
+    local lvl="${1}"
+    local msg="${2}"
+  fi
+  echo "$(date +'%H:%M:%S') [${lvl}] - ${msg}"
+}
+
+die() {
+  if [ "${#}" -eq 1 ]; then
+    local lvl="ERROR"
+    local msg="${1}"
+    local code=1
+  else
+    local lvl="${1}"
+    local msg="${2}"
+    local code="${3}"
+  fi
+  log "${lvl}" "${msg}"
+  exit "${code}"
+}
+
+parse_params() {
+  VERBOSE=0
+
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose)
+      VERBOSE=1
+      ;;
+    --no-color) NO_COLOR=1 ;;
+    -?*) die "Unknown option: ${1}" ;;
+    *) break ;;
+    esac
+    shift
+  done
+}
+
+parse_params "${@}"
+setup_colors
+
+log "Installing Docker Engine"
+
+# Reference: https://docs.docker.com/engine/install/ubuntu/
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
+  run_cmd sudo apt-get remove "${pkg}" -y
+done
+
+safe_rm /var/lib/docker
+safe_rm /var/lib/containerd
+
+safe_rm /etc/apt/sources.list.d/docker.list
+safe_rm /etc/apt/keyrings/docker.asc
+
+run_cmd sudo apt-get install ca-certificates -y
+run_cmd sudo install -m 0755 -d /etc/apt/keyring
+run_cmd sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+run_cmd sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+run_cmd sudo apt-get update
+
+run_cmd sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+log "SUCCESS" "Docker Engine installation complete"
---
+++ bin/install_github_cli.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)
+
+usage() {
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v]
+
+GitHub CLI installer script.
+
+Available options:
+
+-h, --help                          Print this help and exit
+-v, --verbose                       Print script debug info
+EOF
+  exit
+}
+
+run_cmd() {
+  if [ "${VERBOSE:-0}" -eq 1 ]; then
+    "${@}"
+  else
+    "${@}" 2>/dev/null
+  fi
+}
+
+safe_rm() {
+  rm -rf "${1}" 2>/dev/null || sudo rm -rf "${1}" 2>/dev/null || log "WARN" "Failed to remove ${1}"
+}
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  safe_rm "${script_dir}/tmp"
+}
+
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' \
+      BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+log() {
+  if [ "${#}" -eq 1 ]; then
+    local lvl="INFO"
+    local msg="${1}"
+  else
+    local lvl="${1}"
+    local msg="${2}"
+  fi
+  echo "$(date +'%H:%M:%S') [${lvl}] - ${msg}"
+}
+
+die() {
+  if [ "${#}" -eq 1 ]; then
+    local lvl="ERROR"
+    local msg="${1}"
+    local code=1
+  else
+    local lvl="${1}"
+    local msg="${2}"
+    local code="${3}"
+  fi
+  log "${lvl}" "${msg}"
+  exit "${code}"
+}
+
+parse_params() {
+  VERBOSE=0
+
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose)
+      VERBOSE=1
+      ;;
+    --no-color) NO_COLOR=1 ;;
+    -?*) die "Unknown option: ${1}" ;;
+    *) break ;;
+    esac
+    shift
+  done
+}
+
+parse_params "${@}"
+setup_colors
+
+log "Installing GitHub CLI"
+
+# Reference: https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+
+run_cmd sudo mkdir -p -m 755 /etc/apt/keyrings
+run_cmd mkdir "${script_dir}/tmp"
+run_cmd cd "${script_dir}/tmp"
+run_cmd wget -nv -O"keyring.gpg" https://cli.github.com/packages/githubcli-archive-keyring.gpg
+run_cmd sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg <"keyring.gpg"
+run_cmd sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+
+run_cmd sudo mkdir -p -m 755 /etc/apt/sources.list.d
+run_cmd echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list
+
+run_cmd sudo apt update
+
+run_cmd sudo apt install gh -y
+
+log "SUCCESS" "GitHub CLI installation complete"
