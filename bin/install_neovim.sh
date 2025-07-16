f15c231b2ca51ce514d23c0e39a79ce29be4eea5
yifattih <yifattih@proton.me>
2025-07-16 14:37:13 -0500

    ref(installer): remove neovim installation from monolith install-external-packages script to its own script to modularize installer

---
+++ bin/install_neovim.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)
+
+usage() {
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v]
+
+Neovim installer script.
+
+Available options:
+
+-h, --help                          Print this help and exit
+-v, --verbose                       Print script debug info
+EOF
+  exit
+}
+
+run_cmd() {
+  if [ "${VERBOSE:-0}" -eq 1 ]; then
+    "${@}"
+  else
+    "${@}" 2>/dev/null
+  fi
+}
+
+safe_rm() {
+  rm -rf "${1}" 2>/dev/null || sudo rm -rf "${1}" 2>/dev/null || log "WARN" "Failed to remove ${1}"
+}
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+}
+
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' \
+      BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+log() {
+  if [ "${#}" -eq 1 ]; then
+    local lvl="INFO"
+    local msg="${1}"
+  else
+    local lvl="${1}"
+    local msg="${2}"
+  fi
+  echo "$(date +'%H:%M:%S') [${lvl}] - ${msg}"
+}
+
+die() {
+  if [ "${#}" -eq 1 ]; then
+    local lvl="ERROR"
+    local msg="${1}"
+    local code=1
+  else
+    local lvl="${1}"
+    local msg="${2}"
+    local code="${3}"
+  fi
+  log "${lvl}" "${msg}"
+  exit "${code}"
+}
+
+parse_params() {
+  VERBOSE=0
+
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose)
+      VERBOSE=1
+      ;;
+    --no-color) NO_COLOR=1 ;;
+    -?*) die "Unknown option: ${1}" ;;
+    *) break ;;
+    esac
+    shift
+  done
+}
+
+parse_params "${@}"
+setup_colors
+
+log "Installing Neovim from pre-built binaries"
+
+run_cmd curl -LO "https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz"
+safe_rm "${HOME}/bin/nvim"
+run_cmd tar -C "${HOME}/bin/" -xzf "nvim-linux-x86_64.tar.gz"
+run_cmd chown -R "${USER}:${USER}" "${HOME}/bin/nvim-linux-x86_64"
+run_cmd ln -sf "${HOME}/bin/nvim-linux-x86_64/bin/nvim" "${HOME}/bin/nvim"
+safe_rm "nvim-linux-x86_64.tar.gz"
+
+log "SUCCESS" "Neovim installation complete"
